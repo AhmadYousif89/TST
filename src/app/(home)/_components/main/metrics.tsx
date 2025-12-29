866e85e0308fd3c448cfbb40eec250bf861eb9d2
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-12-29 20:46:11 +0200

    Refactor (metrics): - Feeding the metrics component with the neccesary states - Handled hydration error from the Drawer component - Created small utility fns for formating timeLeft value and extracting the current selected mode for better readability.

--- src/app/(home)/_components/main/metrics.tsx
+++ src/app/(home)/_components/main/metrics.tsx
@@ -1,6 +1,7 @@
-import { SettingsIcon } from "@/components/settings.icon";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useState, useEffect } from "react";
+
 import {
   Drawer,
   DrawerClose,
@@ -11,78 +12,117 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { SettingsIcon } from "@/components/settings.icon";
+
+import { ControlPanel } from "./settings";
+import { useEngineState } from "../../engine/engine.context";
+import { formatTime, getModeLabel } from "../../engine/engine-logic";
 
 export const Metrics = () => {
+  const { wpm, accuracy, timeLeft, category, difficulty, mode, status } =
+    useEngineState();
+
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  let timeLeftStyle = "";
+  if (status === "typing" || status === "paused") timeLeftStyle = "text-yellow";
+  if (mode !== "passage" && timeLeft < 10) timeLeftStyle = "text-red";
+  if (mode === "passage" && status === "finished") timeLeftStyle = "text-green";
+
   return (
-    <section className="grid gap-5 border-b pb-4 md:gap-4 xl:grid-flow-col">
-      <div className="flex items-center text-center">
+    <section className="grid gap-5 pb-4 md:gap-4 xl:grid-flow-col">
+      <div className="flex items-center border-b pb-4 text-center xl:border-0 xl:pb-0">
         <div className="flex flex-col gap-2 max-md:grow md:flex-row md:gap-3">
           <span className="text-muted-foreground text-3-mobile md:text-3">
             WPM:
           </span>
-          <span className="text-2">40</span>
+          <span className="text-2">{Math.round(wpm)}</span>
         </div>
         <div className="bg-border mx-5 h-full w-px" />
         <div className="flex flex-col gap-2 max-md:grow md:flex-row md:gap-3">
           <span className="text-muted-foreground text-3-mobile md:text-3">
             Accuracy:
           </span>
-          <span className="text-2 text-red">100%</span>
+          <span
+            className={`text-2 ${status !== "idle" && accuracy === 100 ? "text-green" : accuracy < 100 ? "text-red" : ""}`}
+          >
+            {Math.round(accuracy)}%
+          </span>
         </div>
         <div className="bg-border mx-5 h-full w-px" />
         <div className="flex flex-col gap-2 max-md:grow md:flex-row md:gap-3">
           <span className="text-muted-foreground text-3-mobile md:text-3">
             Time:
           </span>
-          <span className="text-2 text-yellow">00:00</span>
+          <span className={`text-2 ${timeLeftStyle}`}>
+            {formatTime(timeLeft)}
+          </span>
         </div>
       </div>
 
       <div className="flex items-center justify-between gap-4 xl:justify-end">
         <div className="flex h-8 items-center">
           <div className="flex items-center gap-1.5">
-            <span className="text-muted-foreground text-6 md:text-5">
-              Difficulty:{" "}
+            <span className="text-muted-foreground text-6 md:text-5 hidden sm:inline">
+              Category:
             </span>
-            <Badge className="text-muted-foreground text-6 bg-blue-600 px-3 font-medium">
-              Easy
+            <Badge className="text-muted-foreground text-6 bg-blue-600 font-medium capitalize">
+              {category}
             </Badge>
           </div>
           <div className="bg-border mx-4 h-full w-px" />
           <div className="flex items-center gap-1.5">
-            <span className="text-muted-foreground text-6 md:text-5">
-              Mode:{" "}
+            <span className="text-muted-foreground text-6 md:text-5 hidden sm:inline">
+              Difficulty:
             </span>
-            <Badge className="text-muted-foreground text-6 bg-blue-600 px-3 font-medium">
-              Timed (60s)
+            <Badge className="text-muted-foreground text-6 bg-blue-600 font-medium capitalize">
+              {difficulty}
+            </Badge>
+          </div>
+          <div className="bg-border mx-4 h-full w-px" />
+          <div className="flex items-center gap-1.5">
+            <span className="text-muted-foreground text-6 md:text-5 hidden sm:inline">
+              Mode:
+            </span>
+            <Badge className="text-muted-foreground text-6 bg-blue-600 font-medium">
+              {getModeLabel(mode)}
             </Badge>
           </div>
         </div>
 
-        <Drawer>
-          <DrawerTrigger asChild>
-            <Button
-              className="rounded px-3 py-2 md:h-9 md:w-auto md:rounded-md"
-              size="icon-sm"
-            >
-              <SettingsIcon className="size-5" />
-              <span className="hidden md:inline">Settings</span>
-            </Button>
-          </DrawerTrigger>
-          <DrawerContent>
-            <DrawerHeader>
-              <DrawerTitle>Settings</DrawerTitle>
-              <DrawerDescription>
-                Configure your typing settings
-              </DrawerDescription>
-            </DrawerHeader>
-            <DrawerFooter>
-              <DrawerClose asChild>
-                <Button variant="destructive">Close</Button>
-              </DrawerClose>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
+        {hasMounted ? (
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button size="icon">
+                <SettingsIcon className="size-6" />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Settings</DrawerTitle>
+                <DrawerDescription>
+                  Configure your typing settings
+                </DrawerDescription>
+              </DrawerHeader>
+              <ControlPanel />
+              <DrawerFooter>
+                <DrawerClose asChild>
+                  <Button variant="destructive">Close</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        ) : (
+          <Button size="icon">
+            <SettingsIcon className="size-6" />
+          </Button>
+        )}
       </div>
     </section>
   );
